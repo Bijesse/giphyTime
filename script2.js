493a8dffcc9e9ae76a5c1edb62867005a07f5827
Tom Bijesse <tom@bijesse.com>
2016-07-25 14:08:48 -0400

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    var gif = "house";
-    var request = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC";
-        console.log(request);
-        $.getJSON(request, function (data) {
-            console.log(data);
-        });
-});
